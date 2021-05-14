0e01c2b93e44154c74ed3286befce1257fbfc493
kabel2 <pr0verter@gmail.com>
2021-05-14 11:10:17 +0200

    Add example files for react refresh not working

--- basic-host-remote/app1/webpack.config.js
+++ basic-host-remote/app1/webpack.config.js
@@ -1,62 +1,79 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { ModuleFederationPlugin } = require("webpack").container;
+const {ModuleFederationPlugin} = require("webpack").container;
 const path = require("path");
-
+const fs = require('fs');
+const webpack = require('webpack');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 module.exports = {
-  entry: "./src/index",
-  mode: "development",
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    port: 3001,
-  },
-  output: {
-    publicPath: "auto",
-  },
-  module: {
-    rules: [
-      {
-        test: /bootstrap\.js$/,
-        loader: "bundle-loader",
-        options: {
-          lazy: true,
+    entry: "./src/index",
+    mode: "development",
+    devServer: {
+        port: 3201,
+        'host': 'localhost',
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
         },
-      },
-      {
-        test: /\.jsx?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: ["@babel/preset-react"],
-        },
-      },
+    },
+    output: {
+        publicPath: "auto",
+    },
+    module: {
+        rules: [
+            {
+                test: /bootstrap\.js$/,
+                loader: "bundle-loader",
+                options: {
+                    lazy: true,
+                },
+            },
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                options: {
+                    presets: ["@babel/preset-react"],
+                    plugins: [
+                        require.resolve('react-refresh/babel'),
+                    ],
+                },
+            },
+        ],
+    },
+    //http://localhost:3002/remoteEntry.js
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "app1",
+            remotes: {
+                app2: `app2@${getRemoteEntryUrl(3202)}`,
+            },
+            shared: {react: {singleton: true}, "react-dom": {singleton: true}},
+        }),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new ReactRefreshWebpackPlugin({
+            overlay: {
+                sockProtocol: 'ws',
+                sockHost: 'localhost',
+            }
+        })
     ],
-  },
-  //http://localhost:3002/remoteEntry.js
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "app1",
-      remotes: {
-        app2: `app2@${getRemoteEntryUrl(3002)}`,
-      },
-      shared: { react: { singleton: true }, "react-dom": { singleton: true } },
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
 };
 
 function getRemoteEntryUrl(port) {
-  const { CODESANDBOX_SSE, HOSTNAME = '' } = process.env;
+    const {CODESANDBOX_SSE, HOSTNAME = ''} = process.env;
 
-  // Check if the example is running on codesandbox
-  // https://codesandbox.io/docs/environment
-  if (!CODESANDBOX_SSE) {
-    return `//localhost:${port}/remoteEntry.js`;
-  }
+    // Check if the example is running on codesandbox
+    // https://codesandbox.io/docs/environment
+    if (!CODESANDBOX_SSE) {
+        return `//localhost:${port}/remoteEntry.js`;
+    }
 
-  const parts = HOSTNAME.split('-')
-  const codesandboxId = parts[parts.length - 1]
+    const parts = HOSTNAME.split('-')
+    const codesandboxId = parts[parts.length - 1]
 
-  return `//${codesandboxId}-${port}.sse.codesandbox.io/remoteEntry.js`;
+    return `//${codesandboxId}-${port}.sse.codesandbox.io/remoteEntry.js`;
 }
--- basic-host-remote/app2/webpack.config.js
+++ basic-host-remote/app2/webpack.config.js
@@ -1,41 +1,58 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { ModuleFederationPlugin } = require("webpack").container;
+const {ModuleFederationPlugin} = require("webpack").container;
 const path = require("path");
-
+const fs = require('fs');
+const webpack = require('webpack');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 module.exports = {
-  entry: "./src/index",
-  mode: "development",
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    port: 3002,
-  },
-  output: {
-    publicPath: "auto",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: ["@babel/preset-react"],
+    entry: "./src/index",
+    mode: "development",
+    devServer: {
+        port: 3202,
+        'host': 'localhost',
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
         },
-      },
+    },
+    output: {
+        publicPath: "auto",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                options: {
+                    presets: ["@babel/preset-react"],
+                    plugins: [
+                        require.resolve('react-refresh/babel'),
+                    ],
+                },
+            },
+        ],
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "app2",
+            library: {type: "var", name: "app2"},
+            filename: "remoteEntry.js",
+            exposes: {
+                "./Button": "./src/Button",
+            },
+            shared: {react: {singleton: true}, "react-dom": {singleton: true}},
+        }),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new ReactRefreshWebpackPlugin({
+            overlay: {
+                sockProtocol: 'ws',
+                sockHost: 'localhost',
+            }
+        })
     ],
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "app2",
-      library: { type: "var", name: "app2" },
-      filename: "remoteEntry.js",
-      exposes: {
-        "./Button": "./src/Button",
-      },
-      shared: { react: { singleton: true }, "react-dom": { singleton: true } },
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
 };
